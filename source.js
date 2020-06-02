92c6645f1af314ca84f7792e366419aabb3bb6db
yaa1sho <Yaa1sho@yandex.ru>
2020-06-02 21:47:39 +0300

    error whit comments

--- source.js
+++ source.js
@@ -15,28 +15,26 @@ function delDuplicateChar(str) {
         }
 
 
-    let tmp = vec[0];
+    let tmp = vec[0]; // беру первое слово
 
-    for(let i = 0; i < vec[0].length; i++) {
+    for(let i = 0; i < vec[0].length; i++) { // идем по нему
 
         let count = 1;
-        for(let j = 1; j < vec.length; j++)
-            if (vec1[j].indexOf(tmp[i]) === i) count++;
+        for(let j = 1; j < vec.length; j++) // цикл по оставшимся словам
+            if (vec1[j].indexOf(tmp[i]) === i) count++; // если идекс tmp[i] буквы в оставшихся словах
+                                                        // совпадет с индексом этой буквы в первом слове. увеличиваю счетчик
 
-
-        if(count === k)
-            for(let j = 0; j < vec.length; j++){
+        if(count === k) // если количество совпадений равно количеству слов
+            for(let j = 0; j < vec.length; j++){ // в каждом слове удаляю эту букву
 
                let tmp1 = vec[j].substring(0, i);
                tmp1 += vec[j].substring(i + 1, vec[j].length);
 
                vec[j] = tmp1;
             }
-
-
     }
 
-    let res = ''; let j = 0;
+    let res = '';
     for(let i = 0; i < vec1.length; i++)
         str = str.replace(vec1[i],vec[i]);
 
